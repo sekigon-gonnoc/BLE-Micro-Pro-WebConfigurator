3ee1b4e054653f3895091b61688ea91bfa6b71b4
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2020-04-19 21:39:08 +0900

    Add firmware: application v0.5.2

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const app = Elm.App.init({
       "ble_micro_pro_bootloader_0_5_0"
     ],
     applications: [
+      "ble_micro_pro_default_0_5_2",
       "ble_micro_pro_default_0_5_1",
       "ble_micro_pro_default_0_5_0",
       "kugel_default_0_5_1",
